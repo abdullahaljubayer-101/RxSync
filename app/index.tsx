81ecee4027c809ef0819f722c09f5293af648035
Abdullah Al Jubayer <imjubayer08@gmail.com>
2025-10-05 01:04:24 +0600

    Initial commit

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+import "../global.css";
+
+export default function App() {
+  return (
+    <View className="flex-1 items-center justify-center bg-gray-300">
+      <Text className="text-xl font-bold text-gray-500">Hello World!</Text>
+    </View>
+  );
+}
